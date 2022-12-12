71cf2ec1f595d1ad0b39f5aef1aacdd37ce8e288
anastshlyapnik <112941602+anastshlyapnik@users.noreply.github.com>
2022-12-12 04:59:39 +0300

    Multiplication method - Shlyapnikova

---
+++ dz/dz/Shl_dz.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include "Shl_dz.h"
+using namespace std;
+
+
+bool CheckInput(string input) {
+	bool error = false;
+	for (int i = 0; i < input.length(); i++) {
+		if (isdigit(input[i]) == 0) {
+			error = true;
+			break;
+		}
+	}
+	if (error)
+		return 0;
+	return 1;
+}
+
+int HashFunction(string data)
+{
+	int k = std::stoi(data);
+	int N = 13; double A = 0.618033;
+	int h = N * fmod(k * A, 1);
+	return h;
+}
+
+int MultiplMethod(string key)
+{
+	setlocale(LC_ALL, "Rus");
+	if (CheckInput(key)) {
+		return HashFunction(key);
+	}
+	else {
+		cout << "Error!";
+	}
+		
+}
+
+int main() {
+	return 0;
+}
---
+++ dz/dz/Shl_dz.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+bool CheckNumber(string input);
+
+int HashFunction(string data);
+
+int MultiplMethod(string key);
