18d9f28ef98967dec5bbf55dc97a3476651fc946
TricksterLub <102167059+TricksterLub@users.noreply.github.com>
2022-12-12 21:25:54 +0300

    Update rot13.cpp

--- dz/dz/rot13.cpp
+++ dz/dz/rot13.cpp
@@ -1,33 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
-using namespace std;
-using namespace std;
-const int LOWER_A = 97;
-const int LOWER_M = 109;
-const int LOWER_N = 110;
-const int LOWER_Z = 122;
+unsigned int HashRot13(const char* str)
+{
 
-const int UPPER_A = 65;
-const int UPPER_M = 77;
-const int UPPER_N = 78;
-const int UPPER_Z = 90;
+    unsigned int hash = 0;
 
-string rot(string input) {
-    int inputSize = input.size();
-    int index = 0;
-
-    while (index != inputSize) {
-        if (input[index] >= LOWER_A && input[index] <= LOWER_M)
-            input[index] = input[index] + 13;
-        else if (input[index] >= LOWER_N && input[index] <= LOWER_Z)
-            input[index] = input[index] - 13;
-        else if (input[index] >= UPPER_A && input[index] <= UPPER_M)
-            input[index] = input[index] + 13;
-        else if (input[index] <= UPPER_N && input[index] <= UPPER_Z)
-            input[index] = input[index] - 13;
-
-        index++;
+    for (; *str; str++)
+    {
+        hash += (unsigned char)(*str);
+        hash -= (hash << 13) | (hash >> 19);
     }
-    return input;
-}
 
+    return hash;
+
+}
