375bf78389a440ecdcecccd022c2800c3515073e
arisha1-bit <arishastark5@gmail.com>
2022-12-16 23:36:48 +0300

    Update dz.cpp

--- dz/dz/dz.cpp
+++ dz/dz/dz.cpp
@@ -4,22 +4,25 @@
 #include "chrono"
 #include "Shl_dz.h"
 #include "Zhuchkov_AC_21_05.h"
+#include "Header.h"
 using namespace std;
 using namespace chrono;
+using hash_func = string (*) (string p);
 template <typename T>
-void run(T name, string number) {
+void run(hash_func f, string number, string name) {
 	auto start = steady_clock::now();
-	name(number);
+	f(number);
 	auto end = steady_clock::now();
-	cout << "elapsed time: " << duration_cast<microseconds>(end-start).count() << "mcs\n";
+	cout << name << ": elapsed time: " << duration_cast<microseconds>(end-start).count() << "mcs\n";
 }
 int main() {
 	ifstream fin;
 	fin.open("Hash_number");
 	string number;
 	fin >> number;
-	run(metod_square, "12345");
-	run(MultiplMethod, "12345");
-	run(PolinomHash, "1234767677687755456787985");
+	run(metod_square, number, "midsquare technique");
+	run(MultiplMethod, number,"multiplication method");
+	run(PolinomHash, number,"polynomial hashing");
+	run(rot, number, "ROT 13");
 	return 0;
 }
